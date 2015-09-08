414fd6a8d8e802f19a5afb1a9365f89929b1388d
Spencer Seebald <sseebald@gmail.com>
2015-09-08 16:37:47 -0700

    first commit

--- lib/puppet/type/track.rb
+++ lib/puppet/type/track.rb
@@ -9,57 +9,37 @@ Puppet::Type.newtype(:track) do
       track { '/usr/share/foo/foo.war'}
   }
 
-  newparam(:name) do
-    desc "Path of the file name to track."
-    isnamevar
-  end
-
-  #newparam(:recursive, :boolean => true, :parent => Puppet::Parameter::Boolean) do
-  #  desc "Recursively track all the files in this directory"
-  #end
-
-  newproperty(:md5) do |property|
-    # we tell puppet the current property value in the system is always :notcomputed,
-    # then we set the default value so that puppet thinks the manifests want to change
-    # this value to :computed. That's how we force the sync method to run every single time.
+  newproperty(:md5) do |property|  
     defaultto :computed
+    
     def retrieve
-      return :notcomputed
+	    :computed
     end
-
-    def change_to_s(cur,newv)
-      "{md5}#@md5"
-    end
+   
+#    def change_to_s(cur,newv)
+#    end
 
     def sync
-      digest = Digest::MD5.new
+  	  digest = Digest::MD5.new
       File.open(self.resource[:name], 'rb') do |file|
         while content = file.read(4192)
           digest << content
         end
       end
-
+  
       @md5 = digest.hexdigest
+    
+    	self.notice "{md5}#@md5"
     end
+ end
+ 
+  newparam(:name) do
+    desc "Path of the file name to track."
+    isnamevar
   end
 
-  # respond to refresh event, I think
-  def refresh
-    # is this really needed?
-    self.property(:md5).sync
-  end
+	def refresh
+	  self.property(:md5).sync
+	end
 
-  def self.instances
-    # is this really needed?
-    []
-  end
-
-  def output
-    # is this really needed?
-    if self.property(:md5).nil?
-      return nil
-    else
-      return self.property(:md5).output
-    end
-  end
 end
